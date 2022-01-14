59e8eea888dfbde1bada5695f526bf02b33b4741
Soin Roman <soinroma26@gmail.com>
2022-01-14 23:50:06 +0500

    add optimize for files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-var instance = new vidbg(".video", {
+let instance = new vidbg(".video", {
   mp4: "video/PS5.mp4", // URL or relative path to MP4 video
-  webm: "video/PS5.webm", // URL or relative path to webm video
-  poster: "video/posterps5.jpg", // URL or relative path to fallback image
+  poster: "video/posterps5.png", // URL or relative path to fallback image
   overlay: false, // Boolean to display the overlay or not
 });
-var rellax = new Rellax(".console");
+let rellax = new Rellax(".console");
 if (document.body.clientWidth < 576) {
   rellax.destroy();
 }
