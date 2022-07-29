7b19faaa5470e2953f4f08e7b044abc719f0e31b
SoinRoma <soinroma26@gmail.com>
2022-07-29 10:37:11 +0500

    - update images and js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let instance = new vidbg(".video", {
+const instance = new vidbg(".video", {
   mp4: "video/PS5.mp4", // URL or relative path to MP4 video
   poster: "video/posterps5.png", // URL or relative path to fallback image
   overlay: false, // Boolean to display the overlay or not
 });
-let rellax = new Rellax(".console");
+const rellax = new Rellax(".console");
 if (document.body.clientWidth < 576) {
   rellax.destroy();
 }
